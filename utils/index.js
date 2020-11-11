71b89560f8b166c20c8bd5307463fe436e900237
Jundullah H. R <jundihanif77@gmail.com>
2020-11-11 11:24:15 +0700

    Menambahkan direktori untuk utils

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * Require dulu file yang ada di utils,
+ * manual ya...
+ */
+const recursiveJSRequire = require('./recursiveJSRequire');
+
+module.exports = {
+    recursiveJSRequire,
+};
---
+++ utils/recursiveJSRequire.js
@@ -0,0 +1,19 @@
+/**
+ * Secara rekursif, require semua file .js
+ * yang ada di direktori.
+ */
+module.exports = (...dirs) => {
+    const glob = require('glob');
+    const path = require('path');
+    let contains = {};
+
+    dirs.forEach((dir) => {
+        glob.sync(`./${dir}/**/`).forEach(function (subdir) {
+            glob.sync(`./${subdir}/*.js`).forEach(function (file) {
+                Object.assign(contains, require(path.resolve(file)));
+            });
+        });
+    });
+
+    return contains;
+};
