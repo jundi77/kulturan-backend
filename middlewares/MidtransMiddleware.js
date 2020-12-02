0ea2a8fa07015a8805930b457ad0f0da2db243f6
Jundullah H. R <jundihanif77@gmail.com>
2020-12-02 14:48:14 +0700

    debug

--- middlewares/MidtransMiddleware.js
+++ middlewares/MidtransMiddleware.js
@@ -1,8 +1,7 @@
 function checkRequestOrigin(options) {
     return function (req, res, next) {
         if (options && options.log === true) {
-            console.log(req.hostname);
-            console.log(req.body);
+            console.debug(req.headers);
             return res.status(200).json({
                 status: 'success',
                 data: {
