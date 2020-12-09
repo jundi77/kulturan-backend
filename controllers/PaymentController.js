1f7929533b453e7b9d794795983e553b43ce0ba4
Jundullah H. R <jundihanif77@gmail.com>
2020-12-09 14:20:46 +0700

    Fix wrong object to get email

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -46,7 +46,7 @@ function makePembayaran(req, res, snapParameter, data, user, videoID = null) {
                         user.save();
                         global.kulturan.log.discord(
                             req,
-                            `User \`${user.email}\` melakukan pembelian baru dengan id \`${pembayaran._id}\``
+                            `User \`${req.locals.user.data.email}\` melakukan pembelian baru dengan id \`${pembayaran._id}\``
                         );
                         if (err) {
                             console.error(err);
