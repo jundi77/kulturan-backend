bbed4267858939264699e4af4acda42dc552894e
Jundullah H. R <jundihanif77@gmail.com>
2020-12-09 15:34:58 +0700

    Fix wrong format for id

--- models/Pembayaran.js
+++ models/Pembayaran.js
@@ -5,9 +5,9 @@ const generatePaymentId = () => {
     let now = new Date();
     let randStr = require('../config/PaymentNanoIdConfig')(require('nanoid'));
     console.log('nanoid: ' + randStr());
-    return `${now.getFullYear() % 100}${
-        now.getMonth() + 1
-    }${now.getDate()}${randStr()}`;
+    return `${now.getFullYear() % 100}${now.getMonth() + 1}${
+        now.getDate() < 10 ? '0' + now.getDate() : now.getDate()
+    }${randStr()}`;
 };
 
 const PaymentSchema = new Schema(
