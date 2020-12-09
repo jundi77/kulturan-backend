e382e821f75e614f172458244590430fada7f1cd
Jundullah H. R <jundihanif77@gmail.com>
2020-12-09 13:50:00 +0700

    formatting log

--- controllers/LogController.js
+++ controllers/LogController.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 function logOnDiscord(req, msg) {
     axios.post(process.env.DISCORD_LOG_BOT, {
-        content: `${msg}, melalui ${req.ip.address}.`,
+        content: `${msg}, melalui \`${req.ip.address}\`.`,
     });
 }
 
